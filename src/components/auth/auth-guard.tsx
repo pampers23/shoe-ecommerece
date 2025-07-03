ec799d1914128783dea94961a8840e50f0ddcb99
pampers23 <dexterpimss23@gmail.com>
2025-07-03 11:38:36 +0800

    feat: add/protected route login

---
+++ src/components/auth/auth-guard.tsx
@@ -0,0 +1,14 @@
+// import type { ReactNode } from "react";
+// import { Navigate } from "react-router";
+
+// function AuthGuard({ children }: { children: ReactNode }) {
+// //   const { session } = useSession();
+
+//   if (!session) {
+//     return <Navigate to={"/login"} />;
+//   }
+
+//   return <>{children}</>;
+// }
+
+// export default AuthGuard;
---
+++ src/hooks/use-session.ts
@@ -0,0 +1,14 @@
+// import { UserSessionContext } from "@/context/user-session-context";
+// import { useContext } from "react";
+
+// function useSession() {
+//   const context = useContext(UserSessionContext);
+
+//   if (!context) {
+//     throw new Error("Must be inside the context provider!");
+//   }
+
+//   return context;
+// }
+
+// export default useSession;
