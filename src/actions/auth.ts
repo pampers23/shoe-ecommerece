4a5c9bdab61be1f12bb0d887c14bfd296b73e8f7
pampers23 <dexterpimss23@gmail.com>
2025-07-09 12:16:30 +0800

    feat: add/sign up page

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,3 +1,30 @@
-// import { supabase } from "@/lib/client";
-// import { AuthError } from "@supabase/supabase-js";
-// import { RegistrationSchema } from "@/zod-schema";
+import { supabase } from "@/lib/client";
+import { AuthError } from "@supabase/supabase-js";
+import type { SignupSchema } from "@/zod-schema";
+import { toast } from "sonner";
+
+export async function signUp({ firstName, lastName, email, password }:SignupSchema ) {
+    try {
+        const { error } = await supabase.auth.signUp({
+            options: {
+                data: {
+                    firstName,
+                    lastName,
+                },
+            },
+            email,
+            password
+        });
+
+        if (error) {
+            throw new Error(error.message);
+        }
+
+        toast.success("Email verification has been sent!", {
+            description: "Please check your email to confirm your account"
+        })
+    } catch (error) {
+        const err = error as AuthError;
+        toast.error(err.message)
+    }
+}
--- src/zod-schema.ts
+++ src/zod-schema.ts
@@ -1,17 +1,16 @@
 import { z } from "zod";
 
-export const registrationSchema = z
+export const signupSchema = z
   .object({
+    firstName: z.string().min(1, "First name is required"),
+    lastName: z.string().min(1, "Last name is required"),
     email: z.string().email("Invalid email address"),
     password: z.string().min(6, "Password must be at least 6 characters"),
     confirmPassword: z.string(),
-    relationship: z.enum(["mother", "father", "guardian"], {
-      message: "Please select a valid role",
-    }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     path: ["confirmPassword"],
     message: "Passwords do not match",
   });
 
-export type RegistrationSchema = z.infer<typeof registrationSchema>;
+export type SignupSchema = z.infer<typeof signupSchema>;
