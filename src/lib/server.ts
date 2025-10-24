a080e037eac9523a8ff27bb4d2c04b2140cf7444
pampers23 <dexterpimss23@gmail.com>
2025-10-24 15:38:43 +0800

    feat: add/fetching data on profile

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,27 +1,8 @@
-import { createServerClient } from '@supabase/ssr'
-import { parseCookies, setCookie } from '@tanstack/react-start/server'
+import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  return createServerClient(
-    process.env.VITE_SUPABASE_URL!,
-    process.env.VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return Object.entries(parseCookies()).map(
-            ([name, value]) =>
-              ({
-                name,
-                value,
-              }) as { name: string; value: string }
-          )
-        },
-        setAll(cookies) {
-          cookies.forEach((cookie) => {
-            setCookie(cookie.name, cookie.value)
-          })
-        },
-      },
-    }
+  return createBrowserClient(
+    import.meta.env.VITE_SUPABASE_URL!,
+    import.meta.env.VITE_SUPABASE_ANON_KEY!
   )
 }
